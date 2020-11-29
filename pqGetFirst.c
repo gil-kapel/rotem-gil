722031b362915281a681e84f0cdaf3471787db7e
gil-kapel <74864454+gil-kapel@users.noreply.github.com>
2020-11-29 18:51:41 +0200

    Delete pqGetFirst.c

--- pqGetFirst.c
+++
@@ -1,8 +0,0 @@
-#include "priority_queue.h"
-
-PQElement pqGetFirst(PriorityQueue queue){
-    if(queue == NULL || pqGetSize(queue)){
-        return NULL; 
-    }
-    queue->iterator = NULL;
-    return pqGetNext(queue);
