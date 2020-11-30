821ec23d3af1d0097071820e45318d34fa9ecb56
RI-OP <73331173+RI-OP@users.noreply.github.com>
2020-11-30 22:01:03 +0200

    Create date.c

---
+++ date.c
@@ -0,0 +1,118 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "date.h"
+#define MIN_DAY 1
+#define MAX_DAY 30
+#define MIN_MONTH 1
+#define MAX_MONTH 12
+#define DAYS_IN_YEAR 365
+
+struct Date_t
+{
+    int day;
+    int month;
+    int year;
+};
+
+//Returns if day and month are valid
+static bool dateIsLegal(int day, int month) 
+{
+    bool day_val = (day >= MIN_DAY) && (day <= MAX_DAY);
+	bool month_val = (month >= MIN_MONTH) && (month <= MAX_MONTH);
+    return ((day_val) && (month_val));
+}
+
+//Allocates a new date
+Date dateCreate(int day, int month, int year)
+{
+    if(!dateIsLegal(day, month))
+    {
+        return NULL;
+    }
+    Date date = malloc(sizeof(*date));
+    if (!date) 
+    {
+        return NULL;
+    }
+    date->day = day;
+    date->month = month;
+    date->year = year;
+    return date;
+}
+
+//Deallocates an existing Date
+void dateDestroy(Date date)
+{
+    if(!date)
+    {
+        return;
+    }
+    free(date);
+}
+
+//Creates a copy of target Date
+Date dateCopy(Date date)
+{
+    if(!date)
+    {
+        return NULL;
+    }
+    return dateCreate(date->day, date->month, date->year);
+}
+
+//Returns the day, month and year of a date
+bool dateGet(Date date, int* day, int* month, int* year)
+{
+    if((!day) || (!month) || (!year))
+    {
+        return false;
+    }
+    *day = date->day; 
+    *month = date->month; 
+    *year = date->year;
+    return true;
+}
+
+//Convert date to days
+static int dateInDays(Date date)
+{
+    int date_in_days = (date->day) + ((date->month) * MAX_MONTH) + ((date->year) *	DAYS_IN_YEAR);
+	return date_in_days;
+}
+
+//Compares to dates and return which comes first
+int dateCompare(Date date1, Date date2)
+{
+    if((!date1) || (!date2))
+    {
+        return false;
+    }
+    return ((dateInDays(date1)) - (dateInDays(date2)));
+}
+
+
+//Increases the date by one day
+void dateTick(Date date)
+{
+    if(!date)
+    {
+        return;
+    }
+    if(((date->day) == MAX_DAY) && ((date->month) == MAX_MONTH))
+    {
+        date->day = MIN_DAY;
+        date->month = MIN_MONTH;
+        (date->year)++;
+    }
+    else if(((date->day) == MAX_DAY) && ((date->month) < MAX_MONTH))
+    {
+        date->day = MIN_DAY;
+        (date->month)++;
+    }
+    else
+    {
+        (date->day)++;
+    }
+}
+
