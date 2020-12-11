f73520023d2fbc8058c062f430534929a7597323
RI-OP <73331173+RI-OP@users.noreply.github.com>
2020-12-11 21:45:03 +0200

    Create member.c

---
+++ member.c
@@ -0,0 +1,18 @@
+typedef struct Member
+{
+	int member_id;
+	char* member_name;
+	int quantity;
+}Member;
+
+
+//quantity;
+// typedef struct members_list
+// {
+// 	int member_id;
+// 	int quantity;
+// 	char* member_name;
+// 	//PQElement Event;
+// 	struct member* next;
+// }*Member;
+
