3664b1f4f3226571c2e68378e0edec8d5f6d0a77
RI-OP <73331173+RI-OP@users.noreply.github.com>
2020-12-15 23:17:26 +0200

    Update event.c

--- event.c
+++ event.c
@@ -6,15 +6,46 @@
 #include "event_manager.h"
 #include "priority_queue.h"
 #include "date.h"
-
+#include "member.h"
 
 struct Event_t
 {
     char* event_name;
 	int event_id;
-	Date date;
-	//PriorityQueue members_per_event;//?
+    Date date;
+	PriorityQueue members_per_event;//?
 };
+
+static PQElement copy_member(PQElement elem)
+{
+	return memberCopy(elem);
+}
+
+static void free_member(PQElement elem)
+{
+	memberDestroy(elem);
+}
+
+
+static bool equal_member(PQElement elem1,PQElement elem2)
+{
+	return memberCompare(elem1,elem2);
+}
+
+static PQElementPriority copy_member_id(PQElementPriority priority)//check it out
+{
+	return copyMemberId(priority);
+}
+
+static void free_member_id(PQElementPriority priority)
+{
+	freeMemberId(priority);
+}
+
+static int compare_member_id(PQElementPriority priority1, PQElementPriority priority2)
+{
+	return memberIdCompare(priority1, priority2);
+} 
 
 static char* stringCopy(char* new_str)
 {
@@ -26,7 +57,7 @@ static char* stringCopy(char* new_str)
     return copy_str ? strcpy(copy_str, new_str) : NULL;
 }
 
-Event eventCreate(char* name, int id, Date date)//,PriorityQueue members_per_event);
+Event eventCreate(char* name, int id, Date date)//,  members_per_event)// change in em
 {
     Event event = malloc(sizeof(*event));
     if (event == NULL) 
@@ -37,14 +68,12 @@ Event eventCreate(char* name, int id, Date date)//,PriorityQueue members_per_eve
     event->event_name = stringCopy(name);
     if (event->event_name == NULL) 
     {
+        eventDestroy(event);//
         return NULL;
     }
-    if(event->event_name == NULL)
-    {
-        eventDestroy(event);
-    }
     event->event_id = id;
-    event->date = date;///?????
+    PriorityQueue members_per_event = pqCreate(copy_member, free_member, equal_member, copy_member_id, free_member_id, compare_member_id);
+    event->date = date;
     //event->members_per_event = pqCopy(members_per_event);
     return event;
 }
@@ -67,6 +96,7 @@ void eventDestroy(Event event)
         return;
     }
     free(event->event_name);//
+    free(event->members_per_event);//what else?
     free(event);
 }
 
@@ -86,15 +116,14 @@ bool eventCompare(Event event1, Event event2)
     return false;
 }
 
-bool compareEventsId(int id1, int id2)
+int getEventId(Event event)
 {
-    (id1 == id2) ? (return true) : (return false);
-
+    return event->event_id;
 }
 
-char* getEventName(Event event)
+char* getEventName(Event event)//
 {
-       return event->event_name;
+    return event->event_name;
 }
 
 Date getEventdate(Event event)
@@ -102,235 +131,21 @@ Date getEventdate(Event event)
     return event->date;
 }
 
+char* getNameFromEvent(Event event)
+{
+    return stringCopy(event->event_name);
+}
 
-// bool eventGet(Event event, char** name, int* id, Date date)
+// bool compareEventsId(Event event1, Event event2)
 // {
-//     if((name == NULL) || (id == NULL) || (date == NULL))
+//     if ((event1->event_id) == (event2->event_id))
 //     {
-//         return false;
+//         return true;
 //     }
-//     *name = event->event_name; 
-//     *id = event->event_id; 
-//     dateGet(Date date, int* day, int* month, int* year)
-//     *date = event->date;
-//     // date->day = event->date->day;
-//     // (date->month) = event->date->month;
-//     // (date->year) = event->date->year;
-
-//     return true;
-// }
-
-// copy_element(PQElement event)
-// {
-// 	Event new_event = eventCopy((Event)event);
-// 	return new_event;
-
+//     return false;
 // }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-////////////////////////////////////////////////OLD
-#include "event.h"
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "event_manager.h"
-#include "priority_queue.h"
-#include "date.h"
-
-
-struct Event_t
-{
-    char* event_name;
-	int event_id;
-	Date date;
-	//PriorityQueue members_per_event;//?
-};
-
-static char* stringCopy(char* new_str)
-{
-    if(!new_str)
-    {
-        return NULL;
-    }
-    char* copy_str = malloc(strlen(new_str)+1);
-    return copy_str ? strcpy(copy_str, new_str) : NULL;
-}
-
-Event eventCreate(char* name, int id, Date date)//,PriorityQueue members_per_event);
-{
-    Event event = malloc(sizeof(*event));
-    if (event == NULL) 
-    {
-        return NULL;
-    }
-    //event->event_name = malloc(strlen(name)+1);
-    event->event_name = stringCopy(name);
-    if (event->event_name == NULL) 
-    {
-        return NULL;
-    }
-    if(event->event_name == NULL)
-    {
-        eventDestroy(event);
-    }
-    event->event_id = id;
-    event->date = date;///?????
-    //event->members_per_event = pqCopy(members_per_event);
-    return event;
-}
-
-//Creates a copy of target event
-Event eventCopy(Event event)
-{
-    if(event == NULL)
-    {
-        return NULL;
-    }
-    return eventCreate(event->event_name, event->event_id, event->date);
-}
-
-//Deallocates an existing Date
-void eventDestroy(Event event)
-{
-    if(event == NULL)
-    {
-        return;
-    }
-    free(event->event_name);//
-    free(event);
-}
-
-bool eventCompare(Event event1, Event event2)
-{
-    if((event1 == NULL) || (event2 == NULL))
-    {
-        return NULL;
-    }
-    int name_res = strcmp(event1->event_name,event2->event_name);
-    int id_res = ((event1->event_id) - (event2->event_id));
-    int date_res = dateCompare(event1->date, event2->date);
-    if((!name_res) && (!id_res) && (!date_res))
-    {
-        return true;
-    }
-    return false;
-}
-
-char* getEventName(Event event)
-{
-       return event->event_name;
-}
-
-Date getEventdate(Event event)
-{
-    return event->date;
-}
 
 
 // bool eventGet(Event event, char** name, int* id, Date date)
