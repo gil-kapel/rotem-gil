bf06a839ea06373b6a9956d9a9255000834ecff7
RI-OP <73331173+RI-OP@users.noreply.github.com>
2020-12-15 23:18:54 +0200

    Update member.h

--- member.h
+++ member.h
@@ -1,1 +1,41 @@
+#ifndef MEMBER_H_
+#define MEMBER_H_
+
+#include <stdbool.h>
+
+typedef struct Member_t *Member;
+
+Member memberCreate(char* name, int id);//, int amount);//,PriorityQueue members_per_event);
+
+Member memberCopy(Member member);
+
+//Deallocates an existing Member
+void memberDestroy(Member member);
+
+bool memberCompare(Member member1, Member member2);
+
+int* getMemberId(Member member);
+
+char* getMemberName(Member member);
+
+// int getMemberAmount(Member member);
+
+char* getNameFromMember(Member member);
+
+int* copyMemberId(int* member_id);
+
+void freeMemberId(int* member_id);
+
+int* copyAmount(int* amount);
+
+void amountDestroy(int* amount);
+
+int amountCompare(int* amount1, int* amount2);
+
+int memberIdCompare(int* member_id1, int* member_id2);
+
+
+
+#endif //MEMBER_H_
+
 
