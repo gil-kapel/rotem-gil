45bc833c8763a45d6060ec07aac010469c8517ef
RI-OP <73331173+RI-OP@users.noreply.github.com>
2020-12-15 23:16:28 +0200

    Update date.c

--- date.c
+++ date.c
@@ -121,3 +121,107 @@ void dateTick(Date date)
 
 
 
+
+
+//Compares to dates and return which comes first
+// int dateCompare(Date date1, Date date2)
+// {
+//     if((!date1) || (!date2))
+//     {
+//         return false;
+//     }
+//     if(((date1->day) == (date2->day)) && ((date1->month)==(date2->month)) && ((date1->year) == (date2->year)))
+//     {
+//         return EQUAL;
+//     }
+//     if(((date1->year) > (date2->year)) || (((date1->year) == (date2->year)) && ((date1->month) > (date2->month))) || (((date1->year) == (date2->year))
+//          && ((date1->month) == (date2->month)) && ((date1->day) > (date2->day))))
+//     {
+//              return EARLIER;
+//     }
+//     return LATER;
+// }
+
+
+
+    // if (((date->day) < MAX_DAY) || ((date->month) < MAX_MONTH))
+    // {
+    //     (date->day)++;
+    // }
+
+
+// //write
+// void dateTick(Date date)
+// {
+//     if (!date)
+//     {
+//         return;
+//     }
+//     else
+//     {
+        
+//     }
+    
+    
+// }
+
+
+
+
+// static int dateToDays(Date date) {
+// 	int month = monthToInt(date->month);
+// 	return date->day + month*(MAX_DAY - MIN_DAY + 1) +
+//         DAYS_IN_YEAR * date->year;
+// }
+
+// Stack stackCreate(int maxSize) {
+// 	stack->array = malloc(sizeof(int)*maxSize);
+// 	if (stack->array == NULL) {
+// 		free(stack);
+// 		return NULL;
+// 	}
+// 	stack->nextIndex = 0;
+// 	stack->maxSize = maxSize;
+// 	return stack;
+
+
+
+
+
+//Deallocates an existing Date.
+// void dateDestroy(Date date)
+// {
+//     //typedef struct PriorityQueue_t *PriorityQueue;
+//     if (*PriorityQueue == NULL)
+//     {
+//         return;
+//     }
+//     free(date);
+// }
+//dateCopy: Creates a copy of target Date.
+// Date dateCopy(Date date)
+// {
+//     if(date == NULL)
+//     {
+//         return NULL;
+//     }
+//     //strcpy(Date,date);
+//     return date;
+// }
+
+
+ 
+
+ // static bool dateRead(Date date)
+// {
+// 	if (!date)
+//     {
+// 		return false;
+// 	}
+// 	if (scanf("%d %d %d", &(date->day), date->month, &(date->year)) != 3)
+//     {
+// 		return false;
+// 	}
+// 	return dateIsValid(date);
+// }
+
