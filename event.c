c256d91195ab06b43da936febf1bfcf48b2d3aff
RI-OP <73331173+RI-OP@users.noreply.github.com>
2020-12-19 19:17:32 +0200

    Update event.c

--- event.c
+++ event.c
@@ -26,7 +26,6 @@ static void free_member(PQElement elem)
 	memberDestroy(elem);
 }
 
-
 static bool equal_member(PQElement elem1,PQElement elem2)
 {
 	return memberCompare(elem1,elem2);
@@ -47,24 +46,24 @@ static int compare_member_id(PQElementPriority priority1, PQElementPriority prio
 	return memberIdCompare(priority1, priority2);
 } 
 
-static char* stringCopy(char* new_str)
+static char* eventNameCopy(char* new_str)
 {
-    if(!new_str)
+    if(new_str == NULL)
     {
         return NULL;
     }
-    char* copy_str = malloc(strlen(new_str)+1);
-    return copy_str ? strcpy(copy_str, new_str) : NULL;
+    char* copy_str = malloc(strlen(new_str) + 1);
+    return (copy_str != NULL) ? strcpy(copy_str, new_str) : NULL;
 }
 
-Event eventCreate(char* name, int id, Date date)//,  members_per_event)// change in em
+Event eventCreate(char* name, int id, Date date)
 {
     Event event = malloc(sizeof(*event));
     if (event == NULL) 
     {
         return NULL;
     }
-    event->event_name = stringCopy(name);
+    event->event_name = eventNameCopy(name);
     if (event->event_name == NULL) 
     {
         eventDestroy(event);//
@@ -72,8 +71,12 @@ Event eventCreate(char* name, int id, Date date)//,  members_per_event)// change
     }
     event->event_id = id;
     event->members_per_event = pqCreate(copy_member, free_member, equal_member, copy_member_id, free_member_id, compare_member_id);
+    if(event->members_per_event == NULL)
+    {
+        eventDestroy(event);
+        return NULL;
+    }
     event->date = dateCopy(date);
-    //event->members_per_event = pqCopy(members_per_event);
     return event;
 }
 
@@ -84,20 +87,22 @@ Event eventCopy(Event event)
     {
         return NULL;
     }
-    return eventCreate(event->event_name, event->event_id, event->date);
+    Event event_copy = eventCreate(event->event_name, event->event_id, event->date);
+    event_copy->members_per_event = pqCopy(event->members_per_event);
+    return event_copy;
 }
 
 //Deallocates an existing Date
 void eventDestroy(Event event)
 {
-    if(event == NULL)
-    {
-        return;
-    }
-    free(event->date);
-    free(event->event_name);//
-    free(event->members_per_event);//what else?
-    free(event);
+        if(event == NULL)
+        {
+            return;
+        }
+        dateDestroy(event->date);
+        free(event->event_name);
+        pqDestroy(event->members_per_event);
+        free(event);
 }
 
 bool eventCompare(Event event1, Event event2)
@@ -106,7 +111,7 @@ bool eventCompare(Event event1, Event event2)
     {
         return NULL;
     }
-    int name_res = strcmp(event1->event_name, event2->event_name);
+    int name_res = strcmp(event1->event_name,event2->event_name);
     int id_res = ((event1->event_id) - (event2->event_id));
     int date_res = dateCompare(event1->date, event2->date);
     if((!name_res) && (!id_res) && (!date_res))
@@ -121,18 +126,30 @@ int getEventId(Event event)
     return event->event_id;
 }
 
-char* getEventName(Event event)
+char* getEventName(Event event)//
 {
+    if(event == NULL)
+    {
+        return NULL;
+    }
     return event->event_name;
 }
 
 PriorityQueue getMembersPerEvent(Event event)
 {
+    if(event == NULL)
+    {
+        return NULL;
+    }
     return event->members_per_event;
 }
 
 Date getEventdate(Event event)
 {
+    if(event == NULL)
+    {
+        return NULL;
+    }
     return event->date;
 }
 
@@ -140,4 +157,3 @@ char* getNameFromEvent(Event event)
 {
     return event->event_name;
 }
-
